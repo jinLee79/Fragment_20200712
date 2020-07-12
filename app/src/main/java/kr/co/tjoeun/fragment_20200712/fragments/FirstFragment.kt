d83283ca595c821e95742a8fb7af9983a5fc0ccb
jslee <etoileettoi@gmail.com>
2020-07-12 14:42:56 +0900

    프래그먼트 클래스 / XML 연결

--- app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
@@ -1,9 +1,22 @@
 package kr.co.tjoeun.fragment_20200712.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20200712.R
 
 class FirstFragment : Fragment() {  //상속은 꼭 생성자()로 만들어줘야 한다.
 
+    override fun onCreateView(
+        inflater: LayoutInflater,  //xml을 객체로 바꿔주는 기능(inflate). ListView에서도 썼었음
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)  //null 넣었던 ListView와 달리 container, false 넣어주기
+
+    }
 
 
 
